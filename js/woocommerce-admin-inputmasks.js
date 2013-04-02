4536afc1ce51cb9cf79cf1fed3dd4cc152f03d8c
Karzin <Karzin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-02 05:04:25 +0000

    - Added support to number of decimals - Adjusted bug for newer versions of woocommerce, where i convert input of number type to text type
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-admin-input-masks/tags/1.0@690457 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/woocommerce-admin-inputmasks.js
@@ -0,0 +1,85 @@
+jQuery(document).ready(function(e){    
+    handlePriceMasks();
+});
+
+function getPriceInputs(woocommerceVariationBoxes){
+    var inputs=[];
+    for(var i=0;i<woocommerceVariationBoxes;i++){
+        inputs.push(jQuery('input[name="variable_price['+i+']"]'));
+        inputs.push(jQuery('input[name="variable_sale_price['+i+']"]'));
+    }
+    return inputs
+}
+
+function cloneInputs(inputs){
+    var editedPriceInputs=[];
+    
+    for(var i in inputs){
+        var inputInfs = new Object();
+        
+        if(!inputs[i].hasClass('original')){
+            inputs[i].addClass('original');
+            inputs[i].addClass('id'+i);
+            inputs[i].attr('bound','id'+i);
+            inputs[i].addClass('priceInput');
+            
+            var originalPriceInput = inputs[i];
+            var cloned = originalPriceInput.clone();
+            cloned.removeClass('original');
+            cloned.addClass('cloned');
+            cloned.removeAttr('name');
+            cloned.insertAfter(originalPriceInput);
+            inputInfs.original = originalPriceInput;
+            inputInfs.cloned = cloned;
+        }
+        
+        editedPriceInputs[i]=inputInfs;
+    }
+    return editedPriceInputs;
+}
+
+function configMasks(editedPriceInputs){    
+    function onClonedKeyup(cloned,original){
+        var unMasked = cloned.autoNumeric('get')
+        original.attr('value',unMasked)
+    }
+    
+    for(var i in editedPriceInputs){
+        var cloned = editedPriceInputs[i].cloned;
+        var originalPriceInput = editedPriceInputs[i].original;
+        
+        if(cloned){
+            cloned.autoNumeric('init',{
+                aSep:admin_inputmasks_vars.woocommerce_price_thousand_sep,
+                aDec:admin_inputmasks_vars.woocommerce_price_decimal_sep,
+                aSign:'',
+                pSign:''
+            }); 
+
+            cloned.on('keyup',function(){
+                var bound = jQuery(this).attr('bound');
+                var originalPriceInput = jQuery('.priceInput[bound="'+bound+'"]').not(jQuery(this))           
+                onClonedKeyup(jQuery(this),originalPriceInput);                        
+            })        
+        }
+    }
+}
+
+/**
+ * Gerencia as mascaras dos inputs (preco, datas, etc...)
+ */
+function handlePriceMasks(){   
+    
+    function realHandlePriceMasks(){
+        var woocommerceVariation=jQuery(".woocommerce_variation.wc-metabox");        
+        var originalPriceinputs=getPriceInputs(woocommerceVariation.length);
+        var editedPriceInputs = cloneInputs(originalPriceinputs);
+        jQuery('.priceInput.original').hide();
+        configMasks(editedPriceInputs);
+    }
+    
+    jQuery(".woocommerce_variation.wc-metabox").livequery(function(){
+       realHandlePriceMasks();
+    });
+    
+}
