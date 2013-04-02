7bc0e67d709ac7337910a07a5cc42af8eda88cce
Karzin <Karzin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-02 05:04:25 +0000

    - Added support to number of decimals - Adjusted bug for newer versions of woocommerce, where i convert input of number type to text type
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-admin-input-masks/tags/1.1@690457 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ WoocommerceAdminInputmasks.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Plugin Name: Woocommerce admin input masks
+Plugin URI: http://plugins.svn.wordpress.org/woocommerce-admin-input-masks/trunk/
+Description: It masks some woocommerce inputs on the admin side. For the moment, only the price
+Version: 1.0
+Author: Origgami
+Author URI: http://www.origgami.com.br
+License: GPL2
+*/
+
+
+
+class WoocommerceAdminInputmasks {
+//put your code here
+    
+    public function __construct() {
+         if(!function_exists('_log')){
+            function _log( $message ) {
+                if( WP_DEBUG === true ){
+                    if( is_array( $message ) || is_object( $message ) ){
+                        error_log( print_r( $message, true ) );
+                    } else {
+                        error_log( $message );
+                    }
+                }
+            }
+        }
+        
+        
+       
+        //_log(MYPLUGINNAME_PATH);
+        $this->addWPActions();
+    }
+    
+   
+    
+    public function loadAdminJsAndCSS($hook){        
+        /*wp_register_script( 'price-format_js', plugins_url('js/jquery.price_format.1.7.min.js', __FILE__),array('jquery') );
+        wp_enqueue_script( 'price-format_js' );*/
+        
+       
+        
+        if(get_post_type()=='product'){
+            //wp_localize_script('mylib', 'WPULRS', array( 'siteurl' => get_option('siteurl') ));
+            
+            
+            wp_register_script( 'autonumeric_js', plugins_url('js/autonumeric.js', __FILE__),array('jquery') );
+            wp_enqueue_script( 'autonumeric_js' );
+			
+			wp_register_script( 'priceformat_js', plugins_url('js/jquery.price_format.1.7.min.js', __FILE__),array('jquery') );
+            wp_enqueue_script( 'priceformat_js' );
+
+            wp_register_script( 'livequery_js', plugins_url('js/livequery.js', __FILE__),array('jquery') );
+            wp_enqueue_script( 'livequery_js' );
+
+            wp_register_script( 'woocommerce-admin-inputmasks_js', plugins_url('js/woocommerce-admin-inputmasks.js', __FILE__),array('jquery') );
+            wp_enqueue_script( 'woocommerce-admin-inputmasks_js' );
+            
+            wp_localize_script('woocommerce-admin-inputmasks_js', 'admin_inputmasks_vars', array(
+                    'woocommerce_price_thousand_sep' => get_option('woocommerce_price_thousand_sep'),
+                    'woocommerce_price_decimal_sep' => get_option('woocommerce_price_decimal_sep'),
+					'woocommerce_price_num_decimals' => get_option('woocommerce_price_num_decimals')
+                )
+            );
+        }
+        //_log('teste: '.  get_post_type());
+        
+        
+        
+    }
+    
+    protected function addWPActions(){  
+        add_action('admin_enqueue_scripts', array($this,'loadAdminJsAndCSS'));   
+        
+        /*add_action('init', array($this, "init"));
+        add_action('wp_enqueue_scripts', array($this,'loadFrontEndJsAndCSS'));
+        add_action('admin_enqueue_scripts', array($this,'loadAdminJsAndCSS'));       
+        add_action('admin_init', array($this,'admin_init'));
+        add_action('admin_menu', array($this,'admin_menu'));
+        add_action('save_post', array($this,"save_post"));
+        add_action("manage_posts_custom_column",  array($this,"manage_posts_custom_column"));
+        add_action('wp_login_failed', array($this,'my_front_end_login_fail') );  // hook failed login
+        add_action('wp_print_scripts', array($this,'wpPrintScripts') );
+        add_action('widgets_init', array($this,'widgetsInit') );     
+        add_action('wp_head',array($this,'wpHead'));*/
+    }
+    
+}
+
+$wcAdminInputmasks = new WoocommerceAdminInputmasks();
+?>
---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Plugin Name ===
+Contributors: karzin
+Tags: woocommerce, mask, input, format, admin
+Requires at least: 3.0.1
+Tested up to: 3.5
+Stable tag: 1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+It masks some woocommerce inputs on the admin side. For the moment, only the price.
+
+== Description ==
+
+Woocommerce simply doesn't use any type of mask on the admin side. So while you are registering your products, you can mistype your price by accident, for example typing a letter instead of a number, or using a wrong decimal separator.
+
+Whenever it's possible, this plugin creates masks using native Woocommerce configurations that are already in use on the front end, so it can work on the admin side too.
+
+[WooCommerce](http://wordpress.org/extend/plugins/woocommerce/) must be installed and active.
+
+### Description in portuguese: ###
+
+O plugin do Woocommerce simplesmente n&atilde;o usa nenhuma m&aacute;scara para os campos que estao no admin. Ent&atilde;o as vezes sem querer, voc&ecirc; pode acabar digitando o pre&ccedil;o de um produto errado, colocando uma letra no lugar de um n&uacute;mero ou usando uma v&iacute;rgula no lugar de um ponto.
+
+Sempre que poss&iacute;vel, esse plugin cria m&aacute;scaras usando as pr&oacute;prias configura&ccedil;&otilde;es do Woocommerce que j&aacute; funcionam no front end, de maneira que funcionem tamb&eacute;m no admin
+
+#### Credits: ####
+[Autonumeric jQuery plugin](http://www.decorplanit.com/plugin/)
+
+== Installation ==
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= What type of inputs are supported? =
+
+It works only for prices, for now
+
+== Screenshots ==
+
+1. Type comfortably on the admin side using masks in your favor
+2. Whenever it's possible, this plugin creates masks using native Woocommerce configurations that are already in use on the front end, so they can work on the admin side too.
+
+== Changelog ==
+
+= 1.0 =
+* Plugin's creation. 
